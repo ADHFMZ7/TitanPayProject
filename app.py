07cb5bba6c7546f7910f5be121a21dbe453b5e7a
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-10-01 02:12:24 -0700

    added account model

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+from dataclasses import dataclass
 import sqlite3
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash, get_flashed_messages
 
 app = Flask(__name__)
-
-db = {}
+app.secret_key = "asdfgjgh"
+accounts = {}
 
 # @app.route('/')
 # def index():
@@ -18,8 +19,7 @@ def login():
         username = request.form.get("username") 
         password = request.form.get("password")
         if request.form.get("signup") or not valid(username, password):
-            global a; a = "Account does not exist. Please create one."
-        
+            flash("Login Failed. Please Sign Up First")
             return redirect("/signup") 
         else:
             print("success")
@@ -34,10 +34,18 @@ def signup():
         phone = request.form["phone"]
         country= request.form["country"]
         address= request.form["address"]
-        db[user] = pword
-        print(user, pword, name, phone, country, address) 
+        accounts[user] = Account(user, pword, name, phone, country, address)
         return redirect("\login") 
     return render_template("signup.html")
 
 def valid(username, password):
-    return username in db and db[username] == password 
+    return username in accounts.keys() and accounts[username].pword == password 
+
+@dataclass
+class Account:
+    name    : str
+    pword   : str
+    name    : str
+    phone   : str
+    country : str 
+    address : str
